ef261139c7a1a11d7f8cd91315e0e12b8856e2ad
Greg <gregoryjb@rocketmail.com>
2015-04-22 08:48:28 -0700

    Added window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include "dance.h"
+
+Fl_Double_Window* win;
 
 using namespace std;
 
 int main()
 {
+	win = make_window();
+	win->show();
+	Fl::run();
+
 	cout << "Hello World" << endl;
 	cout << "This is a test of github";
 }
