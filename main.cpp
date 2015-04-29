481deb2ab2a966811abf3911b29808362678a35b
Greg <gregoryjb@rocketmail.com>
2015-04-29 08:31:22 -0700

    Set up images in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,24 @@ Fl_Double_Window* win;
 // Creates an array of images we will use for our backgrounds
 Fl_JPEG_Image* i_background [2];
 
+// Arrow key images
+Fl_PNG_Image* i_key_up;
+Fl_PNG_Image* i_key_down;
+Fl_PNG_Image* i_key_left;
+Fl_PNG_Image* i_key_right;
+
 // Sets our image variables to actual image files
 void loadImages()
 {
+	// Backgrounds
 	i_background[0] = new Fl_JPEG_Image("res/bg/bg_1.jpg");
 	i_background[1] = new Fl_JPEG_Image("res/bg/bg_2.jpg");
+	
+	// Keys
+	i_key_up = new Fl_PNG_Image("res/keys/up_key.png");
+	i_key_down = new Fl_PNG_Image("res/keys/down_key.png");
+	i_key_left = new Fl_PNG_Image("res/keys/left_key.png");
+	i_key_right = new Fl_PNG_Image("res/keys/right_key.png");
 }
 
 // Opens a new level
@@ -18,6 +31,7 @@ void loadImages()
 void loadLevel(int levelNum)
 {
 	box_background->image(i_background[levelNum]);
+	box_key->image(i_key_up);
 }
 
 int main()
